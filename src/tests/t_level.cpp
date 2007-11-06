2c0fbd0ed11b16638c2ed654e4764832f6d10a1b
(no author) <(no author)@7e96f2c5-ff98-4bf4-9c58-b8ea98d1b7b6>
2007-11-06 17:15:40 +0000

    Added test for Level which doesn't compile at present
    git-svn-id: svn://localhost/home/nikhil/repository/ricochet/trunk@880 7e96f2c5-ff98-4bf4-9c58-b8ea98d1b7b6

---
+++ src/tests/t_level.cpp
@@ -0,0 +1,49 @@
+#include<iostream>
+
+#include<SDL/SDL.h>
+
+#include "resource.h"
+#include "level.h"
+#include "levelparser.h"
+
+int main() {
+    SDL_Surface *screen;
+    bool loopRunning = true;
+
+    if(SDL_Init(SDL_INIT_VIDEO) != 0) {
+        std::cout<<"Can't initialize SDL"<<SDL_GetError()<<std::endl;
+        exit(1);
+    }
+    atexit(SDL_Quit);
+
+    screen = SDL_SetVideoMode(600, 500, 32, SDL_HWSURFACE|SDL_DOUBLEBUF);
+
+    if (screen == NULL) {
+        std::cout<<"Unable to set video mode: "<<SDL_GetError()<<std::endl;
+        exit(1);
+    }
+
+    SDL_WM_SetCaption("Blocks Test", NULL);
+
+    rtResource::init();
+    rtLevelParser::init();
+    rtPhoton p(rtBlock::DOWN, 325, 10);
+    
+    rtLevel *lvl = rtLevelParser::getLevel(0);
+    SDL_Event event;
+    while(loopRunning)
+    {
+        while(SDL_PollEvent(&event))
+        {
+            if (SDL_QUIT == event.type) loopRunning = false;
+            if (SDL_KEYDOWN == event.type && SDLK_ESCAPE == event.key.keysym.sym) loopRunning = false;
+            
+        }
+        SDL_FillRect(screen, NULL, 0x0);
+
+        lvl->display(screen);
+        SDL_Flip(screen);
+        SDL_Delay(5);
+    }
+    rtResource::cleanup();
+}
