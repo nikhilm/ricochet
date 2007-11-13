ad6ccf514ce138fe750a257d2609120c233c46ec
(no author) <(no author)@7e96f2c5-ff98-4bf4-9c58-b8ea98d1b7b6>
2007-11-13 15:54:21 +0000

    Added testing level 3
    git-svn-id: svn://localhost/home/nikhil/repository/ricochet/trunk@925 7e96f2c5-ff98-4bf4-9c58-b8ea98d1b7b6

--- src/tests/t_level.cpp
+++ src/tests/t_level.cpp
@@ -29,7 +29,7 @@ int main() {
     rtLevelParser::init();
     rtPhoton p(rtBlock::DOWN, 325, 10);
     
-    rtLevel *lvl = rtLevelParser::getLevel(1);
+    rtLevel *lvl = rtLevelParser::getLevel(2);
     
     SDL_Event event;
     while(loopRunning)
