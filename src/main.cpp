176ed8153938a45cf28077ef476057c1bb56e93d
(no author) <(no author)@7e96f2c5-ff98-4bf4-9c58-b8ea98d1b7b6>
2007-11-21 12:11:59 +0000

    Replaced direct display call with variable. NOTE: Not sure if this is really needed, investigating nasty bug
    git-svn-id: svn://localhost/home/nikhil/repository/ricochet/trunk@962 7e96f2c5-ff98-4bf4-9c58-b8ea98d1b7b6

--- src/main.cpp
+++ src/main.cpp
@@ -26,81 +26,84 @@
 #include "textutil.h"
 #include "state.h"
 #include "levelparser.h"
+#include "game.h"
 
-class rtGame {
-    rtState * currentState;
+rtGame::rtGame() {
+    currentState = NULL;
+    screen = NULL;
+    gameRunning = false;
+    stateChanged = false;
+}
     
-    SDL_Surface * screen;
-    
-    bool gameRunning;
-    
-public:
-    
-    enum { SCREEN_WIDTH=800, SCREEN_HEIGHT=600 };
-    
-    void run() {
-        //initialize SDL
-        if(SDL_Init(SDL_INIT_VIDEO) != 0) {
-            std::cout<<"Can't initialize SDL"<<SDL_GetError()<<std::endl;
-            exit(1);
-        }
-        atexit(SDL_Quit);
+void rtGame::run() {
+    //initialize SDL
+    if(SDL_Init(SDL_INIT_VIDEO) != 0) {
+        std::cout<<"Can't initialize SDL"<<SDL_GetError()<<std::endl;
+        exit(1);
+    }
+    atexit(SDL_Quit);
 
-        screen = SDL_SetVideoMode(800, 600, 32, SDL_HWSURFACE|SDL_DOUBLEBUF);
+    screen = SDL_SetVideoMode(800, 600, 32, SDL_HWSURFACE|SDL_DOUBLEBUF);
 
-        if (screen == NULL) {
-            std::cout<<"Unable to set video mode: "<<SDL_GetError()<<std::endl;
-            exit(1);
-        }
+    if (screen == NULL) {
+        std::cout<<"Unable to set video mode: "<<SDL_GetError()<<std::endl;
+        exit(1);
+    }
 
-        SDL_WM_SetCaption("Ricochet", NULL);
-        
-        //initialize resources, levels and text
-        if(!rtResource::init()) {
-            std::cerr<<"Could not load required game resources.\n";
-            exit(1);
-        }
-        atexit(rtResource::cleanup);
+    SDL_WM_SetCaption("Ricochet", NULL);
     
-        if(!rtLevelParser::init()) {
-            std::cerr<<"Could not load levels.\n";
-            exit(0);
-        }
-        
-        if(!rtTextUtil::init()) {
-            std::cerr<<"Error loading font rendering system. Check that SDL_ttf is installed and fonts are in the right place\n";
-            exit(1);
-        }
-        atexit(rtTextUtil::cleanup);
+    //initialize resources, levels and text
+    if(!rtResource::init()) {
+        std::cerr<<"Could not load required game resources.\n";
+        exit(1);
+    }
+    atexit(rtResource::cleanup);
 
-        //begin game
-        gameRunning = true;
-        
-        SDL_Event event;
-        while(gameRunning)
+    if(!rtLevelParser::init()) {
+        std::cerr<<"Could not load levels.\n";
+        exit(0);
+    }
+    
+    if(!rtTextUtil::init()) {
+        std::cerr<<"Error loading font rendering system. Check that SDL_ttf is installed and fonts are in the right place\n";
+        exit(1);
+    }
+    atexit(rtTextUtil::cleanup);
+
+    //begin game
+    gameRunning = true;
+    changeState(new rtStartState(this));
+    
+    SDL_Event event;
+    while(gameRunning)
+    {
+        while(SDL_PollEvent(&event))
         {
-            while(SDL_PollEvent(&event))
-            {
-                if(currentState->handleEvent(event))
-                    ;
-                else if (SDL_QUIT == event.type) gameRunning = false;
-                else if (SDL_KEYDOWN == event.type && SDLK_ESCAPE == event.key.keysym.sym) gameRunning = false;
-            }
-
-            currentState->update();
-            currentState->display(screen);
-            
-            SDL_Flip(screen);
+            if(currentState->handleEvent(event))
+                ;
+            else if (SDL_QUIT == event.type) gameRunning = false;
+            else if (SDL_KEYDOWN == event.type && SDLK_ESCAPE == event.key.keysym.sym) gameRunning = false;
         }
-    }
-    
-    void changeState(State * s) {
-        delete currentState;
-        currentState = s;
-        currentState->firstDisplay(screen);
-    }
-};
+        
+        if(stateChanged) {
+            currentState->firstDisplay(screen);
+            stateChanged = false;
+        }
 
-void main() {
+        currentState->update();
+        currentState->display(screen);
+        
+        SDL_Flip(screen);
+    }
+}
+
+void rtGame::changeState(rtState * s) {
+    if(currentState != NULL)
+        delete currentState;
+    currentState = s;
+    stateChanged = true;
+}
+
+int main() {
     rtGame().run();
 }
