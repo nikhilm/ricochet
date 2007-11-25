581ff73c9a7866363ba863bcfb7ecda28ea4f81a
(no author) <(no author)@7e96f2c5-ff98-4bf4-9c58-b8ea98d1b7b6>
2007-11-25 15:07:59 +0000

    Modified Menu to Ricochet naming and to use rtTextUtil
    git-svn-id: svn://localhost/home/nikhil/repository/ricochet/trunk@984 7e96f2c5-ff98-4bf4-9c58-b8ea98d1b7b6

--- src/menu.cpp
+++ src/menu.cpp
@@ -1,5 +1,5 @@
 /******************************
- * menu.h - a menu framework
+ * menu.h - a menu framework as long as textutil.h is also available
  * Independent framework. Can be
  * used in other projects.
  *
@@ -10,156 +10,134 @@
  */
 
 #include "menu.h"
+#include "textutil.h"
 
-namespace NMUtils {
-    char * MenuItem::fontName = "../data/Vera.ttf";
-    TTF_Font * MenuItem::font = loadFont(MenuItem::fontName, 20);
     
-    MenuItem::MenuItem(int x, int y, char * text, void (*callback)(SDL_Event &)) {
-        itemText = text;
-        actionCallback = callback;
-        currentState = NORMAL;
+rtMenuItem::rtMenuItem(int x, int y, char * text, void (*callback)(SDL_Event &)) {
+    itemText = text;
+    actionCallback = callback;
+    currentState = NORMAL;
 
-        setX(x);
-        setY(y);
-        setDimensions(300, 75);
-        setBorderWidth(5);
+    setX(x);
+    setY(y);
+    setDimensions(300, 75);
+    setBorderWidth(5);
 
-        //default white and black theme
-        SDL_Color white = createColor(255, 255, 255);
-        SDL_Color black = createColor(0, 0, 0);
-        SDL_Color red = createColor(255, 0, 0);
-        setForeground(white);
-        setBackground(black);
-        setBorder(white);
+    //default white and black theme
+    SDL_Color white = createColor(255, 255, 255);
+    SDL_Color black = createColor(0, 0, 0);
+    SDL_Color red = createColor(255, 0, 0);
+    setForeground(white);
+    setBackground(black);
+    setBorder(white);
 
-        setForegroundHover(red);
-        setBackgroundHover(black);
-        setBorderHover(red);
+    setForegroundHover(red);
+    setBackgroundHover(black);
+    setBorderHover(red);
 
 
+}
+
+SDL_Color rtMenuItem::createColor(int r, int g, int b) {
+    SDL_Color col;
+    col.r = r;
+    col.g = g;
+    col.b = b;
+    return col;
+}
+
+SDL_Rect rtMenuItem::createRect(int x, int y, int w, int h) {
+    SDL_Rect r;
+    r.x = x;
+    r.y = y;
+    r.w = w;
+    r.h = h;
+    return r;
+}
+
+SDL_Rect rtMenuItem::getBackgroundRect() {
+    return createRect(oX + borderWidth, oY + borderWidth, width - 2*borderWidth, height - 2*borderWidth);
+}
+
+Uint32 rtMenuItem::getMappedColor(SDL_Color col) {
+    return SDL_MapRGB(surf->format, col.r, col.g, col.b);
+}
+
+TTF_Font * rtMenuItem::loadFont(char * file, int size) {
+    //try loading SDL_ttf
+    if(!TTF_WasInit() && TTF_Init() == -1) {
+        cerr<<"Error initializing SDL_ttf library, cannot draw text. Error: "<<TTF_GetError()<<endl;
     }
 
-    SDL_Color MenuItem::createColor(int r, int g, int b) {
-        SDL_Color col;
-        col.r = r;
-        col.g = g;
-        col.b = b;
-        return col;
+    TTF_Font *f = TTF_OpenFont(file, size);
+    if(!f) {
+        cout<<"Error loading font file"<<TTF_GetError()<<"\n";
     }
+    return f;
+}
 
-    SDL_Rect MenuItem::createRect(int x, int y, int w, int h) {
-        SDL_Rect r;
-        r.x = x;
-        r.y = y;
-        r.w = w;
-        r.h = h;
-        return r;
+void rtMenuItem::setText(char *text) {
+    itemText = text;
+}
+
+void rtMenuItem::setAction(void (*callback)(SDL_Event &)) {
+    actionCallback = callback;
+}
+
+void rtMenuItem::setDimensions(int w, int h) {
+    width = w;
+    height = h;
+}
+
+void rtMenuItem::display(SDL_Surface *screen) {
+    surf = screen;
+
+    if(currentState == NORMAL)
+        displayNormal();
+    else if(currentState == HOVER)
+        displayHover();
+}
+
+bool rtMenuItem::pointInsideThis(int pX, int pY) {
+    return ( pX >= oX && pX <= (oX + width) && pY >= oY && pY <= (oY + height) );
+}
+
+void rtMenuItem::handle(SDL_Event &event) {
+    if(event.type == SDL_MOUSEBUTTONUP && event.button.button == SDL_BUTTON_LEFT
+            && pointInsideThis(event.button.x, event.button.y)) {
+        actionCallback(event);
     }
-
-    SDL_Rect MenuItem::getBackgroundRect() {
-        return createRect(oX + borderWidth, oY + borderWidth, width - 2*borderWidth, height - 2*borderWidth);
-    }
-
-    Uint32 MenuItem::getMappedColor(SDL_Color col) {
-        return SDL_MapRGB(surf->format, col.r, col.g, col.b);
-    }
-
-    TTF_Font * MenuItem::loadFont(char * file, int size) {
-        //try loading SDL_ttf
-        if(!TTF_WasInit() && TTF_Init() == -1) {
-            cerr<<"Error initializing SDL_ttf library, cannot draw text. Error: "<<TTF_GetError()<<endl;
+    else if(event.type == SDL_MOUSEMOTION) {
+        if(pointInsideThis(event.motion.x, event.motion.y)) {
+            currentState = HOVER;
         }
-
-        TTF_Font *f = TTF_OpenFont(file, size);
-        if(!f) {
-            cout<<"Error loading font file"<<TTF_GetError()<<"\n";
-        }
-        return f;
-    }
-
-    void MenuItem::setText(char *text) {
-        itemText = text;
-    }
-
-    void MenuItem::setAction(void (*callback)(SDL_Event &)) {
-        actionCallback = callback;
-    }
-
-    void MenuItem::setDimensions(int w, int h) {
-        width = w;
-        height = h;
-    }
-
-    void MenuItem::display(SDL_Surface *screen) {
-        surf = screen;
-
-        if(currentState == NORMAL)
-            displayNormal();
-        else if(currentState == HOVER)
-            displayHover();
-    }
-
-    bool MenuItem::pointInsideThis(int pX, int pY) {
-        return ( pX >= oX && pX <= (oX + width) && pY >= oY && pY <= (oY + height) );
-    }
-
-    void MenuItem::handle(SDL_Event &event) {
-        if(event.type == SDL_MOUSEBUTTONUP && event.button.button == SDL_BUTTON_LEFT
-                && pointInsideThis(event.button.x, event.button.y)) {
-            actionCallback(event);
-        }
-        else if(event.type == SDL_MOUSEMOTION) {
-            if(pointInsideThis(event.motion.x, event.motion.y)) {
-                currentState = HOVER;
-            }
-            else {
-                currentState = NORMAL;
-            }
+        else {
+            currentState = NORMAL;
         }
     }
+}
 
-    void MenuItem::drawBorder() {
-        SDL_FillRect(surf, &createRect(oX, oY, width, height), getMappedColor((currentState == NORMAL ? borderNormal : borderHover)));
-        //fill in the remaining with black
-        SDL_FillRect(surf, &getBackgroundRect(), getMappedColor(createColor(0, 0, 0)));
+void rtMenuItem::drawBorder() {
+    SDL_FillRect(surf, &createRect(oX, oY, width, height), getMappedColor((currentState == NORMAL ? borderNormal : borderHover)));
+    //fill in the remaining with black
+    SDL_FillRect(surf, &getBackgroundRect(), getMappedColor(createColor(0, 0, 0)));
 
-    }
+}
 
-    void MenuItem::displayNormal() {
-        drawBorder();
-        SDL_FillRect(surf, &getBackgroundRect(), getMappedColor(backgroundNormal));
-        drawText();
-    }
+void rtMenuItem::displayNormal() {
+    drawBorder();
+    SDL_FillRect(surf, &getBackgroundRect(), getMappedColor(backgroundNormal));
+    drawText();
+}
 
-    void MenuItem::displayHover() {
-        drawBorder();
-        SDL_FillRect(surf, &getBackgroundRect(), getMappedColor(backgroundHover));
-        drawText();
-    }
+void rtMenuItem::displayHover() {
+    drawBorder();
+    SDL_FillRect(surf, &getBackgroundRect(), getMappedColor(backgroundHover));
+    drawText();
+}
 
-    void MenuItem::drawText() {
-        SDL_Surface * textSurf;
-        if(!(textSurf = TTF_RenderText_Solid(font, itemText, ( currentState == NORMAL ? foregroundNormal : foregroundHover ))))
-            cout<<TTF_GetError();
-
-        //calculate the centre
-        int textSurfWidth = textSurf->w;
-        int textSurfHeight = textSurf->h;
-
-        int thisCenterX = oX + width/2;
-        int thisCenterY = oY + height/2;
-
-        SDL_Rect r;
-        r.x = thisCenterX - textSurfWidth/2;
-        r.y = thisCenterY - textSurfHeight/2;
-        r.w = textSurfWidth;
-        r.h = textSurfHeight;
-
-        SDL_BlitSurface(textSurf, NULL, surf, &r);
-
-        SDL_FreeSurface(textSurf);
-    }
-
-};
+void rtMenuItem::drawText() {
+    //calculate the centre
+    rtTextUtil::render(itemText.c_str(), red, RT_MEDIUM_FONT, surf, oX + width/2, oY + height/2, rtTextUtil::ALIGN_CENTER, (currentState == HOVER ? true : false)); //last argument is bold
+}
 
--- src/menu.h
+++ src/menu.h
@@ -9,234 +9,221 @@
  * Licensed under the GNU GPL
  */
 
-#ifndef NMUTILS_MENUITEM_H
-#define NMUTILS_MENUITEM_H
+#ifndef RT_rtMenuItem_H
+#define RT__rtMenuItem_H
 
 #include<iostream>
 #include<vector>
 #include<SDL/SDL.h>
-#include<SDL/SDL_ttf.h>
 
-using namespace std;
+/**
+* rtMenuItem is a class implementing a menu item in SDL.
+* It requires the SDL and SDL_ttf libraries to be installed.
+*
+* rtMenuItem can be used independently or through the Menu class.
+* rtMenuItem is quite customisable and allows inheritance to add custom behaviour
+* such as the use of images or fancy effects.
+*
+* Most commonly you will just want to use rtMenuItem as is, specifying only the text
+* and the callback.
+* The callback should be a function with the following prototype
+* <code>
+* void <function name>(SDL_Event &)
+* </code>
+*
+* If using rtMenuItem on its own you should ensure that your event loop passes
+* all events to its handle() method and that you call rtMenuItem's display() method.
+*
+* Here is an example
+* <code>
+* //other includes
+* #include "menu.h"
+*
+* void main() {
+*     ... initialization code...
+*     rtMenuItem item(100, 50, "Click Here", helloWorld);
+*     
+*     //begin loop
+*     while(true) {
+*     while(SDL_PollEvent(&event)) {
+*         item.handle(event);
+*         item2.handle(event);
+*        
+*     }
+*     item.display(screen);
+*     ... more code...
+* }
+* </code>
+*
+* To customise the Look 'N' Feel of the rtMenuItem reimplement the
+* displayNormal() and displayHover() methods.
+* Draw on the SDL_Surface surf which is a class member.
+*/         
+class rtMenuItem {
+private:
 
-namespace NMUtils {
+    enum State { NORMAL, HOVER};
+
+    State currentState;
+    
+    std::string itemText;
+    void (*actionCallback)(SDL_Event &);
+
+    //colours
+    SDL_Color foregroundNormal, backgroundNormal, borderNormal, foregroundHover, backgroundHover, borderHover;
+
+    int width, height, borderWidth;
+
+    //X and Y offsets set by Constructor 
+    //display surface, set by display before calling other display functions
+    int oX, oY;
+    SDL_Surface * surf;
+
+    Uint32 getMappedColor(SDL_Color);
+
+    SDL_Rect getBackgroundRect();
+
+protected:
     /**
-     * MenuItem is a class implementing a menu item in SDL.
-     * It requires the SDL and SDL_ttf libraries to be installed.
-     *
-     * MenuItem can be used independently or through the Menu class.
-     * MenuItem is quite customisable and allows inheritance to add custom behaviour
-     * such as the use of images or fancy effects.
-     *
-     * Most commonly you will just want to use MenuItem as is, specifying only the text
-     * and the callback.
-     * The callback should be a function with the following prototype
-     * <code>
-     * void <function name>(SDL_Event &)
-     * </code>
-     *
-     * If using MenuItem on its own you should ensure that your event loop passes
-     * all events to its handle() method and that you call MenuItem's display() method.
-     *
-     * Here is an example
-     * <code>
-     * //other includes
-     * #include "menu.h"
-     *
-     * void main() {
-     *     ... initialization code...
-     *     MenuItem item(100, 50, "Click Here", helloWorld);
-     *     
-     *     //begin loop
-     *     while(true) {
-     *     while(SDL_PollEvent(&event)) {
-     *         item.handle(event);
-     *         item2.handle(event);
-     *        
-     *     }
-     *     item.display(screen);
-     *     ... more code...
-     * }
-     * </code>
-     *
-     * To customise the Look 'N' Feel of the MenuItem reimplement the
-     * displayNormal() and displayHover() methods.
-     * Draw on the SDL_Surface surf which is a class member.
-     */         
-    class MenuItem {
-    private:
+    * Draws the border around the rtMenuItem
+    */
+    void drawBorder();
+    /**
+    * Draws the text
+    */
+    void drawText();
+    /**
+    * Draws the menu item in the 'normal' state when the mouse is not over it.
+    */
+    void displayNormal();
+    /**
+    * Draws the menu item in the 'hover' state when the mouse is over it.
+    */
+    void displayHover();
 
-        enum State { NORMAL, HOVER};
+public:
+    rtMenuItem(int x, int y, char * , void (*callback)(SDL_Event &));
 
-        State currentState;
-        
-        char * itemText;
-        void (*actionCallback)(SDL_Event &);
+    static SDL_Color createColor(int r, int g, int b);
+    static SDL_Rect createRect(int, int, int, int);
 
-        //colours
-        SDL_Color foregroundNormal, backgroundNormal, borderNormal, foregroundHover, backgroundHover, borderHover;
+    void setText(std::string text);
+    void setAction(void (*callback)(SDL_Event &));
+    void setDimensions(int, int);
 
-        int width, height, borderWidth;
+    void display(SDL_Surface *);
 
-        static char * fontName;
-        static TTF_Font * font;
+    void handle(SDL_Event&);
 
-        //X and Y offsets set by Constructor 
-        //display surface, set by display before calling other display functions
-        int oX, oY;
-        SDL_Surface * surf;
+    bool pointInsideThis(int, int);
 
-        Uint32 getMappedColor(SDL_Color);
-
-        SDL_Rect getBackgroundRect();
-
-        static TTF_Font *loadFont(char *, int);
-
-    protected:
-        /**
-         * Draws the border around the MenuItem
-         */
-        void drawBorder();
-        /**
-         * Draws the text
-         */
-        void drawText();
-        /**
-         * Draws the menu item in the 'normal' state when the mouse is not over it.
-         */
-        void displayNormal();
-        /**
-         * Draws the menu item in the 'hover' state when the mouse is over it.
-         */
-        void displayHover();
-
-    public:
-        MenuItem(int x, int y, char * , void (*callback)(SDL_Event &));
-
-        static SDL_Color createColor(int r, int g, int b);
-        static SDL_Rect createRect(int, int, int, int);
-
-        void setText(char *);
-        void setAction(void (*callback)(SDL_Event &));
-        void setDimensions(int, int);
-
-        void display(SDL_Surface *);
-
-        void handle(SDL_Event&);
-
-        bool pointInsideThis(int, int);
-
-        //setters
-        void setBackground(SDL_Color col) {
-            backgroundNormal = col;
-        };
-
-        void setForeground(SDL_Color col) {
-            foregroundNormal = col;
-        };
-
-        void setBorder(SDL_Color col) {
-            borderNormal = col;
-        };
-
-        void setBackgroundHover(SDL_Color col) {
-            backgroundHover = col;
-        };
-
-        void setForegroundHover(SDL_Color col) {
-            foregroundHover = col;
-        };
-
-        void setBorderHover(SDL_Color col) {
-            borderHover = col;
-        };
-
-        void setBorderWidth(int width) {
-            borderWidth = width;
-        };
-
-        void setX(int x) {
-            oX = x;
-        };
-
-        void setY(int y) {
-            oY = y;
-        };
-
-        void setWidth(int w) {
-            width = w;
-        };
-
-        void setHeight(int h) {
-            height = h;
-        };
-
-        int getWidth() {
-            return width;
-        };
-
-        int getHeight() {
-            return height;
-        };
-
-        void setFontSize(int size) {
-            font = loadFont(fontName, size);
-        };
+    //setters
+    void setBackground(SDL_Color col) {
+        backgroundNormal = col;
     };
 
+    void setForeground(SDL_Color col) {
+        foregroundNormal = col;
+    };
 
-    class Menu {
-    private:
-        vector<MenuItem> menuItems;
-        int x, y;
+    void setBorder(SDL_Color col) {
+        borderNormal = col;
+    };
 
-        int paddingY;
+    void setBackgroundHover(SDL_Color col) {
+        backgroundHover = col;
+    };
 
-    public:
-        Menu(int X, int Y) {
-            x = X;
-            y = Y;
-            paddingY = 10;
-        };
+    void setForegroundHover(SDL_Color col) {
+        foregroundHover = col;
+    };
 
-        Menu() {
-            x = 0;
-            y = 0;
-            paddingY = 10;
-        };
+    void setBorderHover(SDL_Color col) {
+        borderHover = col;
+    };
 
-        void setX(int X) {
-            x = X;
-        };
+    void setBorderWidth(int width) {
+        borderWidth = width;
+    };
 
-        void setY(int Y) {
-            y = Y;
-        };
+    void setX(int x) {
+        oX = x;
+    };
 
-        void addItem(char * text, void (*callback)(SDL_Event &)) {
-            addItem(MenuItem(0, 0, text, callback));
-        };
+    void setY(int y) {
+        oY = y;
+    };
 
-        void addItem(MenuItem m) {
-            m.setX(x);
-            menuItems.push_back(m);
-        };
+    void setWidth(int w) {
+        width = w;
+    };
 
-        void display(SDL_Surface * surf) {
-            int dispY = y;
-            for(unsigned int i = 0; i < menuItems.size(); ++i) {
-                menuItems[i].setY(dispY);
-                menuItems[i].display(surf);
+    void setHeight(int h) {
+        height = h;
+    };
 
-                dispY += menuItems[i].getHeight() + paddingY;
-            }
-        };
+    int getWidth() {
+        return width;
+    };
 
-        void handle(SDL_Event & event) {
-            for(unsigned int i = 0; i < menuItems.size(); ++i) {
-                menuItems[i].handle(event);
-            }
-        };
+    int getHeight() {
+        return height;
+    };
+
+};
+
+
+class Menu {
+private:
+    vector<rtMenuItem> rtMenuItems;
+    int x, y;
+
+    int paddingY;
+
+public:
+    Menu(int X, int Y) {
+        x = X;
+        y = Y;
+        paddingY = 10;
+    };
+
+    Menu() {
+        x = 0;
+        y = 0;
+        paddingY = 10;
+    };
+
+    void setX(int X) {
+        x = X;
+    };
+
+    void setY(int Y) {
+        y = Y;
+    };
+
+    void addItem(char * text, void (*callback)(SDL_Event &)) {
+        addItem(rtMenuItem(0, 0, text, callback));
+    };
+
+    void addItem(rtMenuItem m) {
+        m.setX(x);
+        rtMenuItems.push_back(m);
+    };
+
+    void display(SDL_Surface * surf) {
+        int dispY = y;
+        for(unsigned int i = 0; i < rtMenuItems.size(); ++i) {
+            rtMenuItems[i].setY(dispY);
+            rtMenuItems[i].display(surf);
+
+            dispY += rtMenuItems[i].getHeight() + paddingY;
+        }
+    };
+
+    void handle(SDL_Event & event) {
+        for(unsigned int i = 0; i < rtMenuItems.size(); ++i) {
+            rtMenuItems[i].handle(event);
+        }
     };
 };
 
